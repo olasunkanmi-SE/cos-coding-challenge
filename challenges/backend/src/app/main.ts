0ee58fe58f3f6212e349638de62f7f7ae98925e2
Olasunkanmi <raymond.oyinlola@gmail.com>
2022-05-06 13:59:02 +0800

    update package json file to accomodate auto build of appplication

--- challenges/backend/src/app/main.ts
+++ challenges/backend/src/app/main.ts
@@ -1,30 +1,46 @@
-import {Container} from "inversify";
-import {ILogger} from "./services/Logger/interface/ILogger";
-import {Logger} from "./services/Logger/classes/Logger";
-import {DependencyIdentifier} from "./DependencyIdentifiers";
-import {AuctionMonitorApp} from "./AuctionMonitorApp";
+import { Container } from "inversify";
+import { ILogger } from "./services/Logger/interface/ILogger";
+import { Logger } from "./services/Logger/classes/Logger";
+import { DependencyIdentifier } from "./DependencyIdentifiers";
+// import { AuctionMonitorApp } from "./AuctionMonitorApp";
+import { InversifyExpressServer } from "inversify-express-utils";
+import bodyParser from "body-parser";
 
 /*
  * Create the DI container.
  */
-const container = new Container({
-    defaultScope: "Singleton",
-});
+const container = new Container();
 
 /*
  * Register dependencies in DI environment.
  */
 container.bind<ILogger>(DependencyIdentifier.LOGGER).to(Logger);
 
+//start the server
+let server = new InversifyExpressServer(container);
+
+server.setConfig((app) => {
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(bodyParser.json());
+});
+
+let serverInstance = server.build();
+serverInstance.listen(3000);
+
+console.log("server is running on port");
 
 /*
  * Inject all dependencies in the application & retrieve application instance.
  */
-const app = container.resolve(AuctionMonitorApp);
+// const app = container.resolve(AuctionMonitorApp);
 
 /*
  * Start the application
  */
-(async () => {
-    await app.start();
-})();
+// (async () => {
+//   await app.start();
+// })();
