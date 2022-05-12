91c9fece3c02b8d9a1d95c0ef3b70bf131d24871
Olasunkanmi <raymond.oyinlola@gmail.com>
2022-05-12 09:06:55 +0800

    check for invalid login in the error middleware

--- challenges/backend/src/app/infrastructure/middleware/error-middleware.ts
+++ challenges/backend/src/app/infrastructure/middleware/error-middleware.ts
@@ -3,10 +3,13 @@ import { Request, Response } from "express";
 import { HttpException } from "./../exceptions/http-exceptions";
 // @ts-ignore
 export function errorMiddleware(error: HttpException, request: Request, response: Response, next: express.NextFunction) {
-  const status = error.status;
-  const message = error.message || "Something went wrong";
-  const axiosRespponse = error.response.status;
-  return response.status(status || axiosRespponse).json({
+  let status = error.status;
+  let message = error.message || "Something went wrong";
+  let axiosStatusResponse = error.response.status;
+  if (axiosStatusResponse === 401) {
+    message = "Invalid usermailId or password";
+  }
+  return response.status(status || axiosStatusResponse).json({
     message,
     status,
   });
